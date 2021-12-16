c148973084671ff45b21883d9f82d58cb8f9db4f
Marisol Rdz <darenkita@gmail.com>
2021-12-16 15:23:28 -0600

    sincronizando repositorios

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { AppUI } from './AppUI';
 
-// const defaultTodos = [
-//   { text: 'Cortar cebolla', completed: true },
-//   { text: 'Tomar el cursso de intro a React', completed: false },
-//   { text: 'Llorar con la llorona', completed: true },
-//   { text: 'LALALALAA', completed: false },
-// ];
+ /*const defaultTodos = [
+   { text: 'Cortar cebolla', completed: true },
+   { text: 'Tomar el cursso de intro a React', completed: false },
+   { text: 'Llorar con la llorona', completed: true },
+   { text: 'LALALALAA', completed: false },
+];*/
 
-function useLocalStorage(itemName, initialValue) {
+/*function useLocalStorage(itemName, initialValue) {
   const localStorageItem = localStorage.getItem(itemName);
   let parsedItem;
   
@@ -31,11 +31,23 @@ function useLocalStorage(itemName, initialValue) {
     item,
     saveItem,
   ];
-}
+}*/
 
 function App() {
  // const [patito, savePatito] = useLocalStorage('PATITO_V1', 'FERNANDO');
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+ 
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  //const [todos, saveTodos] = React.useState(parsedTodos);
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -51,7 +63,13 @@ function App() {
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
     });
-  }
+ }
+ 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
 
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
